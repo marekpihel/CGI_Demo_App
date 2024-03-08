8f28010475ed97d499c5732f079f7b20a52d9f9f
Marek Pihel <marekpihel@gmail.com>
2024-03-08 22:11:58 +0200

    Just barebones project at this point

---
+++ src/main/java/com/example/cgi_demo_app/CgiDemoAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.cgi_demo_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CgiDemoAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CgiDemoAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cgi_demo_app/CgiDemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cgi_demo_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CgiDemoAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
